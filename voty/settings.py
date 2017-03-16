58c4889fb35e9c6483897e678c29467ea5285187
Benjamin Kampmann <ben@gnunicorn.org>
2017-03-16 16:41:40 +0100

    automatic db urls

--- voty/settings.py
+++ voty/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -96,10 +97,7 @@ SITE_ID = 1
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default='sqlite://./db.sqlite3')
 }
 
 

b069db0673a5ead61df4ad2c3c05173b4591ae6f
Benjamin Kampmann <ben@gnunicorn.org>
2017-06-08 22:36:48 +0200

    fix urls

--- voty/initadmin/urls.py
+++ voty/initadmin/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
 	# globals
-    url('^/account/edit$', views.profile_edit, name='profile_edit'),
-    url('^/backoffice/mass_invite$', views.mass_invite, name='mass_invite'),
-    url('^/backoffice/download_csv/(?P<id>.*)$', views.download_csv, name='download_mass_invites'),
+    url('^account/edit$', views.profile_edit, name='profile_edit'),
+    url('^backoffice/mass_invite$', views.mass_invite, name='mass_invite'),
+    url('^backoffice/download_csv/(?P<id>.*)$', views.download_csv, name='download_mass_invites'),
 ]

7ed292489abf18c216d48e5a4e1f28ce40b2bb2d
Benjamin Kampmann <ben@gnunicorn.org>
2017-06-23 23:56:41 +0200

    urlize

--- voty/initadmin/templatetags/markdown.py
+++ voty/initadmin/templatetags/markdown.py
@@ -1,6 +1,7 @@
 from django import template
 from django.conf import settings
 from django.utils.safestring import mark_safe
+from django.utils.html import urlize
 
 import markdown2
 import bleach
@@ -9,6 +10,6 @@ register = template.Library()
 
 @register.filter
 def markdown(text):
-    text = markdown2.markdown(text)
+    text = markdown2.markdown(urlize(text, nofollow=True, autoescape=True))
     html = bleach.clean(text, tags=settings.MARKDOWN_FILTER_WHITELIST_TAGS)
     return mark_safe(bleach.linkify(html))

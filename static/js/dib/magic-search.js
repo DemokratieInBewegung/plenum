ac92acb769809cf00b9af5f0191799bcebfc3b5f
Benjamin Kampmann <ben@gnunicorn.org>
2017-07-13 13:48:55 +0200

    dropdown

--- static/js/dib/magic-search.js
+++ static/js/dib/magic-search.js
@@ -1,20 +1,41 @@
+Vue.component('dropdown', {
+    props: ['options', 'input'],
+    // data: function(){
+    //   return {input: this.input, options: this.options}
+    // },
+    computed: {
+      filtered: function() {
+        // if (!this.input) {
+          return this.options;
+        // }
+      }
+    }
+});
+
 Vue.component('search-bar', {
-  props: ['filters'],
+  props: ['filters', 'searchOptions'],
   data: function() {
     return { filters: this.filters,
-             message: ''
+             curText: '',
+             selected: false,
            }
   },
-  computed: {placeholder: function() {
+  computed: {
+    placeholder: function() {
       return (this.filters && this.filters.length > 0) ? '' : 'Filtern und suchen...';
+    },
+    options: function(){
+      if (!this.selected && this.curText){
+        return this.searchOptions;
+      }
     }
   },
   methods: {
     add: function() {
-      let msg = this.message.trim()
+      let msg = this.curText.trim()
       if (msg) {
         this.filters.push({name: "Suche", value: msg})
-        this.message = ''
+        this.curText = ''
       } else {
         // commit search here.
       }
@@ -28,6 +49,11 @@ var magicSearch = new Vue({
     filters: [
       {name: 'Suche', value: "Yeah"},
       {name: 'Phase', value: 'Angenommen'}
+    ],
+    searchOptions: [
+      {name: 'Bereich', key: 'b'},
+      {name: 'Phase', key: 'f'},
+      {name: 'Freitext Suche', key: 's'}
     ]
   }
 });

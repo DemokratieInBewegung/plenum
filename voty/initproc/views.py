DemokratieInBewegung/plenum

commit abda45b045b6875eea16644e726b14e11f00166c

add final edit and post moderation incl publishing flow

voty/initproc/views.py
@@ -259,11 +259,14 @@ def edit(request, initiative):
 
 
 @login_required
-@can_access_initiative(STATES.PREPARE, 'can_edit')
+@can_access_initiative([STATES.PREPARE, STATES.FINAL_EDIT], 'can_edit')
 def submit_to_committee(request, initiative):
     if initiative.ready_for_next_stage:
-        initiative.state = STATES.INCOMING
+        initiative.state = STATES.INCOMING if initiative.state == STATES.PREPARE else STATES.MODERATION
         initiative.save()
+
+        # make sure moderation starts from the top
+        initiative.moderations.update(stale=True)
 
         messages.success(request, "Deine Initiative wurde angenommen und wird geprüft.")
         initiative.notify_followers(NOTIFICATIONS.INITIATIVE.SUBMITTED, subject=request.user)
@@ -327,7 +330,7 @@ def support(request, initiative):
 
 @require_POST
 @login_required
-@can_access_initiative([STATES.PREPARE, STATES.INCOMING])
+@can_access_initiative([STATES.PREPARE, STATES.INCOMING, STATES.FINAL_EDIT])
 def ack_support(request, initiative):
     sup = get_object_or_404(Supporter, initiative=initiative, user_id=request.user.id)
     sup.ack = True
@@ -405,7 +408,7 @@ def new_proposal(request, form, initiative):
 
 @ajax
 @login_required
-@can_access_initiative('i', 'can_moderate') # must be in discussion
+@can_access_initiative([STATES.INCOMING, STATES.MODERATION], 'can_moderate') # must be in discussion
 @simple_form_verifier(NewModerationForm)
 def moderate(request, form, initiative):
     model = form.save(commit=False)
@@ -414,23 +417,47 @@ def moderate(request, form, initiative):
     model.save()
 
     if request.guard.can_publish(initiative):
-        initiative.supporting.filter(ack=False).delete()
-        initiative.went_public_at = datetime.now()
-        initiative.state = STATES.SEEKING_SUPPORT
-        initiative.save()
+        if initiative.state == STATES.INCOMING:
+            initiative.supporting.filter(ack=False).delete()
+            initiative.went_public_at = datetime.now()
+            initiative.state = STATES.SEEKING_SUPPORT
+            initiative.save()
 
-        messages.success(request, "Initiative veröffentlicht")
-        initiative.notify_followers(NOTIFICATIONS.INITIATIVE.PUBLISHED)
-        initiative.notify_moderators(NOTIFICATIONS.INITIATIVE.PUBLISHED, subject=request.user)
+            messages.success(request, "Initiative veröffentlicht")
+            initiative.notify_followers(NOTIFICATIONS.INITIATIVE.PUBLISHED)
+            initiative.notify_moderators(NOTIFICATIONS.INITIATIVE.PUBLISHED, subject=user)
+            return redirect('/initiative/{}'.format(initiative.id))
 
-        return redirect('/initiative/{}'.format(initiative.id))
+        elif initiative.state == STATES.MODERATION:
+
+            publish = [initiative]
+            if initiative.all_variants:
+                # check the variants, too
+
+                for ini in initiative.all_variants:
+                    if ini.state != STATES.MODERATION or not request.guard.can_publish(ini):
+                        publish = None
+                        break
+                    publish.append(ini)
+
+            if publish:
+                for init in publish:
+                    init.went_to_voting_at = datetime.now()
+                    init.state = STATES.VOTING
+                    init.save()
+                    init.notify_followers(NOTIFICATIONS.INITIATIVE.WENT_TO_VOTE)
+                    init.notify_moderators(NOTIFICATIONS.INITIATIVE.WENT_TO_VOTE, subject=request.user)
+
+                messages.success(request, "Initiative(n) zur Abstimmung frei gegeben.")
+                return redirect('/initiative/{}-{}'.format(initiative.id, initiative.slug))
+
 
     
     return {
         'inner-fragments': {'#moderation-new': "<strong>Eintrag aufgenommen</strong>",
-                            '#moderation-list'.format(initiative.id):
+                            '#moderation-list':
                                 render_to_string("fragments/moderation/list_small.html",
-                                                  context=dict(initiative=initiative),
+                                                  context=dict(moderations=initiative.current_moderations),
                                                   request=request)}
     }
 

templates/fragments/initiative/card.html
@@ -46,6 +46,23 @@
 					</span>
 					{% endifequal %}
 
+					{% ifequal initiative.state 'm' %}
+
+					<span class="badge badge-arrivals">
+
+						{% if user.is_staff %}
+							{% guard should_moderate_initiative initiative %}
+							{% if should_moderate_initiative %}
+							💥💥 Bitte final prüfen  💥💥
+							{% else %}
+							Finale Prüfung
+							{% endif %}
+						{% else %}
+						Finale Prüfung
+						{% endif %}
+			      	</span>
+					{% endifequal %}
+
 					{% ifequal initiative.state 'v' %}
 					<span class="badge badge-vote">
 						Abstimmung

templates/fragments/moderation/full.html
@@ -1,5 +1,6 @@
 {% load avatar_tags %}
 {% load markdown %}
+<div id="{{m.unique_id}}" class="moderation-item">
 
 {% avatar m.user 50 %} {% if m.vote == 'y' %}<span class="badge badge-success"><i class="material-icons">thumb_up</i></span>{% elif m.vote == 'n' %}<span class="badge badge-danger"><i class="material-icons">thumb_down</i></span>{% else %}<span class="badge badge-default"><i class="material-icons">thumbs_updown</i></span>{% endif %}{{m.created_at|timesince}}
 <p>
@@ -13,4 +14,5 @@
 </div>
 <div id="{{m.unique_id}}-new-comment">
     <a data-ajax="true" href="/comment/{{m.type}}/{{m.id}}?fragment=%23{{m.unique_id}}-new-comment" class="btn btn-small btn-outline-primary">Kommentieren</a>
+</div>
 </div>

templates/fragments/moderation/index.html
@@ -1,12 +1,24 @@
-<div id="moderation">
+<div id="moderation" class="moderation alert alert-warning">
 
+	<strong>Moderationsberwertungen</strong>
 	<div id="moderation-list">
-        {% include 'fragments/moderation/list_small.html' %}
+        {% include 'fragments/moderation/list_small.html' with moderations=initiative.current_moderations %}
 	</div>
 
 	{% if request.guard.should_moderate_initiative %}
+		<br/>
 		<div id="moderation-new">
 			<a data-ajax=True href="/initiative/{{initiative.id}}/new_moderation?fragment=%23moderation-new" class="btn btn-outline-primary">Moderationsbewertung eintragen</a>
 		</div>
 	{% endif %}
+
+
+	{% if initiative.stale_moderations.count %}
+		<br>
+		<hr>
+		<br>
+		<strong>Moderation vorheriger Versionen</strong>
+	    {% include 'fragments/moderation/list_small.html' with moderations=initiative.stale_moderations %}
+	{% endif %}
+
 </div>

templates/fragments/moderation/list_small.html
@@ -1,6 +1,6 @@
 {% load avatar_tags %}
-{% for m in initiative.moderations.all %}
-	<div id="{{m.unique_id}}">
+{% for m in moderations %}
+	<div id="{{m.unique_id}}" class="moderation-item">
 	{% avatar m.user 35 %} {% if m.vote == 'y' %}<span class="badge badge-success"><i class="material-icons">thumb_up</i></span>{% elif m.vote == 'n' %}<span class="badge badge-danger"><i class="material-icons">thumb_down</i></span>{% else %}<span class="badge badge-default"><i class="material-icons">thumbs_updown</i></span>{% endif %}{% if m.text %}<a data-ajax="true" href="/initiative/{{initiative.id}}/moderation/{{m.id}}?fragment=%23{{m.unique_id}}"><i class="material-icons">comment</i></a>{% endif %}
 	</div>
 {% empty %}

templates/initproc/index.html
@@ -72,6 +72,14 @@
 						</div>
 						<div class="col-auto">
 						    <label>
+						      	<input name="f" value="m" type="checkbox" {% if 'm' in filters %} checked{% endif %}>
+						      	<span class="badge badge-arrivals">
+						      		Finale Prüfung
+						      	</span>
+						    </label>
+						</div>
+						<div class="col-auto">
+						    <label>
 						      	<input name="f" value="v" type="checkbox" {% if 'v' in filters %} checked{% endif %}>
 						      	<span class="badge badge-vote">
 						      		Abstimmung

templates/initproc/item.html
@@ -48,59 +48,68 @@
     <div class="container">
         <div class="row initiative-state">
             <div class="col-12">
-                {% if initiative.state == 'p' or initiative.state == 'i' %}
-                {% if request.guard.can_edit %}
-                    {% if not initiative.ready_for_next_stage %}
-                    <div class="pre-submit">
-                        <div class="alert alert-info" role="alert">
-                        <h6>Um Deine Initiative einreichen zu können, müssen folgende Bedingungen erfüllt sein:</h6>
-                        <ol>
-                            <li>Alle Textfelder beim Einreichen müssen ausgefüllt sein.</li>
-                            <li>Du musst genau 2 Mitinitiator/innen angeben.</li>
-                            <li>Deine Mitinitiator/innen müssen ihre Mitautorschaft bestätigt haben.</li>
-                        </ol>
-                        </div>
-                        {% endif %}
-                        <form action="/initiative/{{initiative.id}}/edit" style="display:inline"  method="GET">
-                            {% csrf_token %}
-                            <button title="Bearbeiten" class="btn btn-success edit" type="submit">
-                                <i class="material-icons edit">edit</i><span>Bearbeiten</span>
-                            </button>
-                        </form>
-                        {% if initiative.ready_for_next_stage %}
-                        <form action="/initiative/{{initiative.id}}/submit_to_committee" style="display:inline"  method="POST">
-                            {% csrf_token %}
-                            <button title="Einreichen" class="btn btn-success submit" type="submit">
+                {% if initiative.state == 'p' or initiative.state == 'i' or initiative.state == 'e' %}
+                    {% if request.guard.can_edit %}
+                        {% if not initiative.ready_for_next_stage %}
+                        <div class="pre-submit">
+                            <div class="alert alert-info" role="alert">
+                            <h6>Um Deine Initiative einreichen zu können, müssen folgende Bedingungen erfüllt sein:</h6>
+                            <ol>
+                                <li>Alle Textfelder beim Einreichen müssen ausgefüllt sein.</li>
+                                <li>Du musst genau 2 Mitinitiator/innen angeben.</li>
+                                <li>Deine Mitinitiator/innen müssen ihre Mitautorschaft bestätigt haben.</li>
+                            </ol>
+                            </div>
+                            {% endif %}
+                            <form action="/initiative/{{initiative.id}}/edit" style="display:inline"  method="GET">
+                                {% csrf_token %}
+                                <button title="Bearbeiten" class="btn btn-success edit" type="submit">
+                                    <i class="material-icons edit">edit</i><span>Bearbeiten</span>
+                                </button>
+                            </form>
+                            {% if initiative.ready_for_next_stage %}
+                            <form action="/initiative/{{initiative.id}}/submit_to_committee" style="display:inline"  method="POST">
+                                {% csrf_token %}
+                                <button title="Einreichen" class="btn btn-success submit" type="submit">
+                                    <i class="material-icons public">public</i><span>Einreichen</span>
+                                </button>
+                            </form>
+                            {% else %}
+                            <button title="Kann noch nicht eingereicht werden" class="btn btn-success submit" style="display:inline" disabled=True type="submit">
                                 <i class="material-icons public">public</i><span>Einreichen</span>
                             </button>
-                        </form>
-                        {% else %}
-                        <button title="Kann noch nicht eingereicht werden" class="btn btn-success submit" style="display:inline" disabled=True type="submit">
-                            <i class="material-icons public">public</i><span>Einreichen</span>
-                        </button>
-                    </div>
+                        </div>
+                        {% endif %}
                     {% endif %}
                 {% endif %}
 
-                <p>
-                    Diese Initiative <strong class="badge-prepare">wurde noch nicht veröffentlicht</strong>.
-                </p>
-                    {% if request.guard.can_moderate %}
-                        {% include 'fragments/moderation/index.html' %}
-                    {% elif initiative.state == 'i' and request.user.is_staff %}
-                        <div class="alert alert-info">{{request.guard.reason}}</div>
+                {% if initiative.state == 'p' %}
+                    <p>
+                    An diese Initiative <strong class="badge-prepare">wird noch gearbeitet</strong>.
+                    </p>
+                {% elif initiative.state == 'e' %}
+                    <p>
+                    Diese Initiative <strong class="badge-prepare">wird für die Abstimmung finalisiert</strong>.
+                    </p>
+                {% elif initiative.state == 'i' or initiative.state == 'm' %}
+                    <p>
+                    Diese Initiative <strong class="badge-prepare">wird gerade geprüft</strong> und {% if initiative.state == 'i' %} wird dann veröffentlicht {% else %} geht dann in <strong  class="badge-prepare">die Abstimmung</strong>{% endif %}.
+                    </p>
+                    {% if request.user.is_staff %}
+                        {% if request.guard.can_moderate %}
+                            {% include 'fragments/moderation/index.html' %}
+                        {% else %}
+                            <div class="alert alert-info">{{request.guard.reason}}</div>
+                        {% endif %}
                     {% endif %}
                 {% endif %}
-                {% ifequal initiative.state 'm' %}
-                <p>
-                    Diese Initiative <strong class="badge-arrivals">wird gerade moderiert</strong>.
-                </p>
-                {% endifequal %}
+
                 {% ifequal initiative.state 'h' %}
                 <p>
-                    Diese Initiative <strong class="badge-arrivals">ist gerade nicht sichtbar</strong>.
+                    Diese Initiative <strong class="badge-arrivals">ist nicht sichtbar</strong>.
                 </p>
                 {% endifequal %}
+
                 {% ifequal initiative.state 's' %}
                 {% if initiative.absolute_supporters < initiative.quorum %}
                 <p>
@@ -128,13 +137,13 @@
                     Diese Initiative befindet sich <a href="#debate">in Diskussion</a>.
                 </p>
                 {% endifequal %}
-                {% ifequal initiative.state 'e' %}
-                <span class="badge badge-edits">
-                    Finale Überarbeitung
-                </span> {% endifequal %} {% ifequal initiative.state 'v' %}
-                <span class="badge badge-vote">
-                    Abstimmung
-                </span> {% endifequal %} {% ifequal initiative.state 'a' %}
+
+
+                {% ifequal initiative.state 'v' %}
+                    <p>
+                        Über diese Initiative <a href="#voting">wird gerade abgestimmt</a>.
+                    </p>
+                {% endifequal %} {% ifequal initiative.state 'a' %}
                 <span class="badge badge-accepted">
                     Angenommen
                 </span> {% endifequal %} {% ifequal initiative.state 'r' %} {% if initiative.went_to_discussion_at %}
@@ -165,7 +174,7 @@
                     <div class="name">
                         {{ sup.user.get_full_name|default:sup.user.username }}
                     </div>
-                    {% if initiative.state == 'i' or initiative.state == 'p' %}
+                    {% if initiative.state == 'i' or initiative.state == 'p'  or initiative.state == 'e' %}
                     <div class="approval">
                         {% if sup.ack %}
                             <span title="bestätigt" class=""><i class="material-icons done">done</i></span>
@@ -275,6 +284,7 @@
 </div>
 {% endifequal %}
 
+
 {% ifequal initiative.state 'd' %}
 <div id="cta" class="container-fluid cta">
     <div class="container">
@@ -284,6 +294,22 @@
                 <p>Diese Initiative befindet sich in Diskussion.</p>
                 <a href="#discuss" class="btn bg-discussion">
                     Jetzt diskutieren
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+{% endifequal %}
+
+{% ifequal initiative.state 'v' %}
+<div id="cta" class="container-fluid cta">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h6 class="text-muted classification">Noch {{initiative.end_of_this_phase | timeuntil}}</h6>
+                <p>Über diese Initiative wird gerade abgestimmt .</p>
+                <a href="#discuss" class="btn bg-discussion">
+                    Jetzt abstimmen
                 </a>
             </div>
         </div>

voty/initproc/guard.py
@@ -216,7 +216,7 @@ class Guard:
         return init.state == STATES.SEEKING_SUPPORT and self.user.is_authenticated
 
     def _can_moderate_initiative(self, init):
-        if init.state == STATES.INCOMING and self.user.is_staff:
+        if init.state in [STATES.INCOMING, STATES.MODERATION] and self.user.is_staff:
             if init.supporting.filter(user=self.user, initiator=True):
                 self.reason = "Als Mitinitator/in darfst du nicht mit moderieren."
                 return False

commit 0601498d2ef45f4e7253a1072b637e1b5ffa1339

fixed undefined variable bug introduced in abda45b

voty/initproc/views.py
@@ -475,7 +475,7 @@ def moderate(request, form, initiative):
 
             messages.success(request, "Initiative veröffentlicht")
             initiative.notify_followers(NOTIFICATIONS.INITIATIVE.PUBLISHED)
-            initiative.notify_moderators(NOTIFICATIONS.INITIATIVE.PUBLISHED, subject=user)
+            initiative.notify_moderators(NOTIFICATIONS.INITIATIVE.PUBLISHED, subject=request.user)
             return redirect('/initiative/{}'.format(initiative.id))
 
         elif initiative.state == STATES.MODERATION:
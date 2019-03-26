51d2864c8de719ec133764b63020ed6197ee0e48
joriki <joriki27@gmail.com>
2019-03-26 13:14:31 +0100

    also stop database container for backup

--- scripts/anonymize_avatars.sh
+++ scripts/anonymize_avatars.sh
@@ -3,7 +3,7 @@ set -e
 
 # before running this script, check out a version that sets AVATAR_EXPOSE_USERNAMES = False
 docker-compose build web
-docker-compose stop web
+docker-compose stop
 tar cvfz data.tgz data # backup
 docker-compose run web python manage.py anonymize_avatars
-docker-compose up -d web
+docker-compose up -d

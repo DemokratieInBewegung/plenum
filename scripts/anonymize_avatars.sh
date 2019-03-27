c974f817cc82839152176a6ac195c3e96de83344
joriki <joriki27@gmail.com>
2019-03-27 12:32:00 +0100

    replaced command and script by migration

--- scripts/anonymize_avatars.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# before running this script, check out a version that sets AVATAR_EXPOSE_USERNAMES = False
-docker-compose build web
-docker-compose stop
-tar cvfz data.tgz data # backup
-docker-compose run web python manage.py anonymize_avatars
-docker-compose up -d

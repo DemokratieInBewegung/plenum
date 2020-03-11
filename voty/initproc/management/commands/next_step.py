26019c123711728f0346cd7830d949fd2ee46cea
Michael <michael.voss82@gmail.com>
2020-03-11 13:04:37 +0100

    Agorá 2.0
    Basisdemokratietool für Parteistrategiefragen

--- voty/initproc/management/commands/next_step.py
+++ voty/initproc/management/commands/next_step.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
-from voty.initproc.models import Initiative
+from voty.initproc.models import Initiative, Issue
 from voty.initproc.globals import STATES, NOTIFICATIONS
 from django.template.loader import render_to_string
 from django.core.mail import EmailMessage
@@ -32,6 +32,10 @@ class Command(BaseCommand):
             for i in Initiative.objects.filter(state__in=AUTOMATIC_STAGES):
                 if i.ready_for_next_stage and i.end_of_this_phase_date <= date.today():
                     self.advance(i)
+                    
+            for i in Issue.objects.filter(status__in=[STATES.SEEKING_SUPPORT,STATES.DISCUSSION,STATES.MODERATION,STATES.VOTING,STATES.VETO]):
+                if i.end_of_current_phase <= date.today():
+                    self.advance_issue(i)
 
     def advance(self, i):
                 # phases incoming, prepare and moderation are entered through manual action
@@ -83,3 +87,40 @@ class Command(BaseCommand):
                     except Exception as e:
                         print(e)
                         print("Waiting for deadlock, not moving yet")
+            
+    def advance_issue(self, i):
+        # phases prepare and incoming are entered through manual action
+    
+        if i.status == STATES.SEEKING_SUPPORT: # did not get enough suppoters, so close
+            i.status = STATES.COMPLETED
+            i.was_closed_at = datetime.now()
+            i.save()
+            i.notify_followers(NOTIFICATIONS.ISSUE.CLOSED)
+    
+        elif i.status == STATES.DISCUSSION:
+            i.status = STATES.MODERATION
+            i.went_to_final_review_at = datetime.now()
+            i.save()
+            i.notify_final_review()
+    
+        elif i.status == STATES.MODERATION:
+            i.status = STATES.VOTING
+            i.went_to_voting_at = datetime.now()
+            i.save()
+            i.notify_initiators(NOTIFICATIONS.ISSUE.WENT_TO_VOTE)
+    
+        elif i.status == STATES.VOTING:
+            if i.solutions.exclude(status='r').first().rating.count() >= i.voters_quorum:
+                i.status = STATES.VETO
+                i.went_to_veto_phase_at = datetime.now()
+                i.notify_board(NOTIFICATIONS.ISSUE.VOTED)
+            else:
+                i.status = STATES.COMPLETED
+                i.was_closed_at = datetime.now()
+            i.save()
+    
+        elif i.status == STATES.VETO:
+            i.status = STATES.COMPLETED
+            i.was_closed_at = datetime.now()
+            i.save()
+            i.notify_initiators(NOTIFICATIONS.ISSUE.COMPLETED)

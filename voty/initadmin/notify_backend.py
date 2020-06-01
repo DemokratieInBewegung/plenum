f7b3563c3f8470ed44c0b26a9e2655a0aad40db9
Michael <michael.voss82@gmail.com>
2020-06-01 10:25:51 +0200

    mark issue and solution notifications as read

--- voty/initadmin/notify_backend.py
+++ voty/initadmin/notify_backend.py
@@ -29,11 +29,22 @@ def mark_as_read(get_response):
     def middleware(request):
         # from voty.initproc.models import Initiative
         response = get_response(request)
-        if request.user and request.user.is_authenticated and getattr(request, 'initiative', None):
-          init_type = ContentType.objects.get_for_model(request.initiative)
-          q = request.user.notifications.filter(
-              Q(actor_content_type=init_type, actor_object_id=request.initiative.id) | \
-              Q(target_content_type=init_type, target_object_id=request.initiative.id)).mark_all_as_read()
+        if request.user and request.user.is_authenticated:
+            if getattr(request, 'initiative', None):
+              init_type = ContentType.objects.get_for_model(request.initiative)
+              q = request.user.notifications.filter(
+                  Q(actor_content_type=init_type, actor_object_id=request.initiative.id) | \
+                  Q(target_content_type=init_type, target_object_id=request.initiative.id)).mark_all_as_read()
+            if getattr(request, 'issue', None):
+              issue = ContentType.objects.get_for_model(request.issue)
+              q = request.user.notifications.filter(
+                  Q(actor_content_type=issue, actor_object_id=request.issue.id) | \
+                  Q(target_content_type=issue, target_object_id=request.issue.id)).mark_all_as_read()
+            if getattr(request, 'solution', None):
+              solution = ContentType.objects.get_for_model(request.solution)
+              q = request.user.notifications.filter(
+                  Q(actor_content_type=solution, actor_object_id=request.solution.id) | \
+                  Q(target_content_type=solution, target_object_id=request.solution.id)).mark_all_as_read()
 
         return response
 

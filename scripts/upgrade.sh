136844af2cf6b1fb840ffa9586b907c5c6c6dca7
Guido Drehsen <33029486+GuidoDr@users.noreply.github.com>
2020-12-31 23:49:06 +0100

    Create local database migrations

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -6,6 +6,10 @@ cd /code
 echo "Collecting statics" 
 SECRET_KEY=temp_value python manage.py collectstatic -v 0 --clear --noinput
 
+# Create local database migrations
+echo "Create database migrations"
+python manage.py makemigrations
+
 # Apply database migrations
 echo "Apply database migrations"
 python manage.py migrate

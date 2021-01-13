5f7dc9e6b9a85d9c012727ff343ac989fcc014d1
michaelvoss82 <37080006+michaelvoss82@users.noreply.github.com>
2021-01-13 16:37:11 +0100

    same order for solutions and resistances

--- voty/initproc/views.py
+++ voty/initproc/views.py
@@ -545,14 +545,14 @@ def get_issue_resistances(request, issue_id):
 def get_issue_context(issue_id, request):
     context = {}
     context['issue'] = get_object_or_404(Issue, pk=issue_id)
-    context['resistances'] = get_issue_resistances(request, issue_id).order_by('created_at')
+    context['resistances'] = get_issue_resistances(request, issue_id).order_by('-solution__createdate')
     return context
 
 @can_access_issue()
 def issue_item(request, issue, slug=None, archive=False):
     context = dict(issue=issue)
     context['archive'] = archive
-    context['resistances'] = get_issue_resistances(request, issue).order_by('created_at')
+    context['resistances'] = get_issue_resistances(request, issue).order_by('-solution__createdate')
     
     solutions = Solution.objects.filter(issue=issue.id).extra(select={'is_rejected': "status = 'r'"})
     context['solutions'] = solutions.order_by('is_rejected','-createdate')

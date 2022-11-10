6d17d94d4280fe369047e2ce53ffd492393d76fb
Guido Drehsen <guido@drehsen.com>
2022-11-10 15:42:03 +0100

    replace docker-compose with docker compose

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@ set -e
 
 git fetch
 git checkout $TRAVIS_BRANCH
-docker-compose build web
-docker-compose run web bash /code/scripts/upgrade.sh
-docker-compose stop web
-docker-compose up -d web 
+docker compose build web
+docker compose run web bash /code/scripts/upgrade.sh
+docker compose stop web
+docker compose up -d web 
